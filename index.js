c0e6cf1ee6c0d272a996d0dbc870604fba2cca2d
Simphiwe Tebe <simphiwetebe@gmail.com>
2021-12-30 20:16:58 +0200

    make it better

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.use(cors());
 app.use('/posts', postsRoute)
 app.use('/user', userRoute)
 
+app.get('/', (req,res)=>{
+    res.send('Welcome to Instaverse API')
+})
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONN_URL, { useNewUrlParser: true, useUnifiedTopology: true })
