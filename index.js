26761500e045b103305c3cb21cc47fef4fdc7d3c
Simphiwe Tebe <simphiwetebe@gmail.com>
2021-12-30 19:29:22 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postsRoute from './routes/posts.js';
+import userRoute from './routes/users.js';
+
+const app = express();
+app.use(express.json({ limit: 3000000 }));
+app.use(cors());
+
+//Middlewares
+app.use('/posts', postsRoute)
+app.use('/user', userRoute)
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONN_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=> app.listen(PORT, console.log(`Server running on port: ${PORT}`)))
+.catch(err => console.log(err.message))
+
